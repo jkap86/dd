a2191e58193281af1fb7b6181c7a169301daa017
jkap86 <jkap86@gmail.com>
2022-06-15 09:09:23 -0400

    -

--- client/src/components/leagues.js
+++ client/src/components/leagues.js
@@ -115,8 +115,15 @@ const Leagues = (props) => {
         setLeagues([...l])
     }
 
+    const total_wins = leagues.filter(x => x.isLeagueHidden === false).reduce((acc, cur) => acc + cur.record.wins, 0)
+    const total_losses = leagues.filter(x => x.isLeagueHidden === false).reduce((acc, cur) => acc + cur.record.losses, 0)
+    const total_ties = leagues.filter(x => x.isLeagueHidden === false).reduce((acc, cur) => acc + cur.record.ties, 0)
+    const win_pct = total_wins + total_losses > 0 ? total_wins / (total_wins + total_losses + total_ties) : null
+
     return <>
         <div className="search_wrapper">
+            <h2>{leagues.filter(x => x.isLeagueHidden === false).length} Leagues</h2>
+            <h2>{total_wins}-{total_losses}{total_ties === 0 ? null : `-${total_ties}`} <em>{win_pct.toFixed(4)}</em></h2>
             <Search
                 list={leagues.map(league => league.name)}
                 placeholder="Search Leagues"
